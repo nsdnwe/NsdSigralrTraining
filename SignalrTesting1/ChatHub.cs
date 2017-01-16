006ccab5b9008a8364713730d1b72b49e0e9d677
Niko Wessman <niko.wessman@nsd.fi>
2017-01-16 10:52:42 +0200

    One to One

--- SignalrTesting1/ChatHub.cs
+++ SignalrTesting1/ChatHub.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
 
 namespace SignalrTesting1 {
     public class ChatHub : Hub {
+        private static List<UserConnection> connectionList = new List<UserConnection>();
         public void Hello() {
-            Clients.All.hello("x");
-        } 
-        public void Send(string name, string message) {
-            // Call the broadcastMessage method to update clients.
-            Clients.All.broadcastMessage(name, message);
+            Clients.All.hello("Hello all");
+        }
+
+        public void Send(string sendFromUserId, string sentFromName, string toUserId, string message) {
+            var connections = connectionList.Where(z => z.UserId == toUserId);
+            foreach (var item in connections) { // Same user may use the app from more than one device
+                Clients.Client(item.ConnectionId).broadcastMessage(sentFromName, message);
+            }
+        }
+
+        // Must have userId in querystring! 
+        // See sample web page how to define in JS
+        public override Task OnConnected() {
+            connectionList.Add(new UserConnection() {
+                UserId = Context.QueryString["userId"],
+                ConnectionId = Context.ConnectionId
+            });
+            return base.OnConnected();
         }
     }
 }
---
+++ SignalrTesting1/UserConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalrTesting1 {
+    public class UserConnection {
+        public string UserId { set; get; }
+        public string ConnectionId { set; get; }
+    }
+}
